63aab9d6b36b146fbbed64aaf31ede253591d876
ReushevKirill <reushev.kd@gmail.com>
2025-07-17 17:57:19 +0300

    first commit

---
+++ app/features/email-builder/components/EmailBuilder/index.vue
@@ -0,0 +1,3 @@
+<template>
+  Email Builder
+</template>
---
+++ eslint.config.js
@@ -0,0 +1,21 @@
+import antfu from '@antfu/eslint-config'
+import tailwindPlugin from 'eslint-plugin-tailwindcss'
+
+export default antfu(
+  {
+    vue: true,
+    typescript: true,
+    unocss: false,
+    formatters: true,
+  },
+  {
+    plugins: {
+      tailwindcss: tailwindPlugin,
+    },
+    rules: {
+      'tailwindcss/classnames-order': 'warn',
+      'tailwindcss/no-custom-classname': 'warn',
+      'tailwindcss/no-contradicting-classname': 'error',
+    },
+  },
+)
